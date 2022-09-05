c9f0a014b6358050e63a5c4c7dc9a1542412afbe
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-09-05 11:45:41 +0530

    Update sample.sql

--- db/sample.sql
+++ db/sample.sql
@@ -21,3 +21,30 @@ describe student;
 desc student;
 
 drop table student;
+
+CREATE TABLE student(
+student_ID INT UNSIGNED NOT NULL,
+name VARCHAR(40) NOT NULL,
+major VARCHAR(20),
+grade VARCHAR(5));
+
+ALTER TABLE student ADD  phone INT;
+ALTER TABLE student ADD PRIMARY KEY (student_ID);
+
+alter table student add country varchar(25) NOT NULL DEFAULT 'India';
+
+CREATE TABLE employees(
+employee_id INT,
+last_name VARCHAR(25) NOT NULL,
+email VARCHAR(25),
+CONSTRAINT emp_email_uk UNIQUE(email)
+);
+
+describe employees;
+ALTER TABLE employees
+ALTER last_name SET DEFAULT 'user333';
+
+alter table student drop major;
+desc student;
+
+alter table student alter phone SET DEFAULT  12345;
