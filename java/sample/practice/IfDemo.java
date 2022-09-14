aac4f1d065c97ccf477ff72f13215e30877efa0c
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-09-14 11:45:41 +0530

    Add files via upload

---
+++ java/sample/practice/IfDemo.java
@@ -0,0 +1,14 @@
+package sample.practice;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        int money = 100;
+        int penPrice = 10;
+        if(money >= penPrice){
+            System.out.println("yes u can buy pen ");
+        }
+        else{
+            System.out.println(" u cant buy ");
+        }
+    }
+}
---
+++ java/sample/practice/IncrementDemo.java
@@ -0,0 +1,20 @@
+package sample.practice;
+
+public class IncrementDemo {
+    public static void main(String[] args){
+        int a = 20;
+        // a++; //post increment by 1 
+
+        ++a;//pre increment 
+        System.out.println(a);
+
+
+        int mark = 5;
+        System.out.println(mark++);
+        System.out.println(mark);
+
+        // int b = 5;
+        // b--;
+        // System.out.println(b);
+    }
+}
