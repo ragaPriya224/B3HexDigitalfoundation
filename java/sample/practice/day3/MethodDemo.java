48e643478c9b43df93379844644ff4188809242c
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-09-15 11:07:21 +0530

    Add files via upload

--- java/sample/practice/day3/MethodDemo.java
+++ java/sample/practice/day3/MethodDemo.java
@@ -2,18 +2,19 @@ package sample.practice.day3;
 
 public class MethodDemo {
     public static void main(String[] args) {
-        toSubtract();
-        toAdd();
+        toSubtract("user");
+        toAdd(10, 3);
     }
 
-    public static int toAdd() {
-        int result = 5 + 3;
-System.out.println("hiii");
+    public static int toAdd(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println(result);
         return result;
     }
 
-    public static void toSubtract() {
+    public static void toSubtract(String name) {
         System.out.println(5 - 3);
+        System.out.println(name);
     }
 
 }
