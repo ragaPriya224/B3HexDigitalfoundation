b5e1d47d40ddef77d36f34a7be337f631535eb9e
Bugs Bunny <zzzz@gmail.com>
2022-09-29 09:41:36 +0530

    array object loop demo

--- HTML/code/sample.js
+++ HTML/code/sample.js
@@ -12,14 +12,14 @@ console.log(greeting == habit);//true
 //  confirm("confirm you're a human");
 //  prompt("enter name");
 
-let person = prompt("Please enter your name", "Harry Potter");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
-  text = "Hello " + person + "! How are you today?";
-}
-console.log(text);
+// let person = prompt("Please enter your name", "Harry Potter");
+// let text;
+// if (person == null || person == "") {
+//   text = "User cancelled the prompt.";
+// } else {
+//   text = "Hello " + person + "! How are you today?";
+// }
+// console.log(text);
 
 // FUNCTION 
 
@@ -36,3 +36,24 @@ alert ("Welcome")
 }else{
 alert ("Thank You")
 }
+// array concepts
+let flavours = ['choc','strawberry','vanilla','pista'];
+console.log(flavours[1]); //strawberry
+console.log(flavours[2]); //vanilla
+flavours[2] = "butterscotch";
+console.log(flavours[2]); //butterscotch
+flavours.push("blackcurrent");
+console.log(flavours);
+// flavours.sort();
+console.log(flavours.sort());
+
+// OBJECT
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+console.log("*********");
+console.log(person.eyeColor);//blue
+console.log("*********");
